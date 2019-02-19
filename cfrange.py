eeecd6c06ec76231d4b7515ce47a30f6f59fe8a5
Robert Hoppe <robert.hoppe@nodemash.com>
2019-02-19 13:00:27 +0100

    [TASK] Adjustment of cfrange.py

--- cfrange.py
+++ cfrange.py
@@ -3,6 +3,31 @@ import json
 from io import BytesIO
 
 
+cloudflare_real_ip = [
+    '103.21.244.0/22',
+    '103.22.200.0/22',
+    '103.31.4.0/22',
+    '104.16.0.0/12',
+    '108.162.192.0/18',
+    '131.0.72.0/22',
+    '141.101.64.0/18',
+    '162.158.0.0/15',
+    '172.64.0.0/13',
+    '173.245.48.0/20',
+    '188.114.96.0/20',
+    '190.93.240.0/20',
+    '197.234.240.0/22',
+    '198.41.128.0/17',
+    '2400:cb00::/32',
+    '2606:4700::/32',
+    '2803:f800::/32',
+    '2405:b500::/32',
+    '2405:8100::/32',
+    '2c0f:f248::/32',
+    '2a06:98c0::/29'
+]
+
+
 
 buffer = BytesIO()
 c = pycurl.Curl()
@@ -14,8 +39,11 @@ c.close()
 body = buffer.getvalue()
 json1_data = json.loads(body.decode('iso-8859-1'))
 print('# cloudront dynamic IP range, taken from https://ip-ranges.amazonaws.com/ip-ranges.json')
+
 for majorkey in json1_data['prefixes']:
+
     if 'CLOUDFRONT' in majorkey['service']:
         print("set_real_ip_from\t" + majorkey['ip_prefix'] + ";")
 
-
+for real_ip in cloudflare_real_ip:
+    print("set_real_ip_from\t" + real_ip + ";")
